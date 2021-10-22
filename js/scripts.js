e5bbe6456feb20d351aa66a56a131cf93dd601eb
Ella Tanttu <ellajtanttu@gmail.com>
2021-10-22 12:22:11 -0700

    add basic quiz functionality ui and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,73 @@
+//Business Logic:
+
+let a = 0;
+let b = 0;
+let c = 0;
+
+function resultCount(res) {
+  if (res === "a") {
+    return a += 1;
+  } else if (res === "b") {
+    return b += 1;
+  } else if (res === "c") {
+    return c += 1;
+  }
+};
+
+
+//User Interface Logic:
+$(document).ready(function () {
+  $("#quizForm").submit(function (event) {
+    event.preventDefault();
+    $(".results").show();
+    const expLevel = $("#inputExp").val();
+    console.log("expLevel = " + expLevel);
+    const qOne = $("input:radio[name=q1]:checked").val();
+    console.log("qOne = " + qOne);
+    const qTwo = $("input:radio[name=q2]:checked").val();
+    console.log("qTwo = " + qTwo);
+    const qThr = $("input:radio[name=q3]:checked").val();
+    console.log("qThr = " + qThr);
+    const qFour = $("input:radio[name=q4]:checked").val();
+    console.log("qFour = " + qFour);
+    const qFive = $("input:radio[name=q5]:checked").val();
+    console.log("qFive = " + qFive);
+    const qSix = $("input:radio[name=q6]:checked").val();
+    console.log("qSix = " + qSix);
+    const answersArray = [qOne, qTwo, qThr, qFour, qFive, qSix];
+    console.log("answersArray = " + answersArray);
+    resultCount(expLevel);
+    resultCount(qOne);
+    resultCount(qTwo);
+    resultCount(qThr);
+    resultCount(qFour);
+    resultCount(qFive);
+    resultCount(qSix);
+    console.log("a = " + a + ", b = " + b + ", c = " + c);
+
+    if ((a > b) && (a > c)) {
+      $(".resGo").show();
+    } else if ((b > c) && (b > a)) {
+      $(".resJs").show();
+    } else if ((c > a) && (c > b)) {
+      $(".resCs").show();
+    };
+  });
+});
+
 /*
-BRAINSTORM - PROBLEM SOLVING PREWORK
+UI Logic: When form is submitted, each question returns a single letter value: a, b, or c. 
+Business logic: function that will count the occurrences # of each letter.
+Business logic: function that will compare amounts of each and produce one value based on the highest number: this will be a branching statement.
+UI logic: function that runs the two business logic functions and produces the language name based on the value, shows the corresponding results div. 
+UI logic: Return name of user in the results div
+
+
+
+
+
+----------
+BRAINSTORM - PROBLEM SOLVING PRE-WORK
 
 Three languages:
 A Go
@@ -40,4 +108,3 @@ C Microsoft
 
 
 */
-
